be37bc28ddd0a1d63ca836c17b4c34ea35c5b815
akleefel <akleefel@gmail.com>
2017-12-07 18:48:16 -0800

    added ratio_graphic.R

---
+++ src/ratio_graphic.R
@@ -0,0 +1,10 @@
+#! /usr/bin/env Rscript
+# sumary_graphic.R
+# Alexander Kleefeldt Dec, 2017
+
+# This script reads in the summary of rations produced by ratio_summary.R and creates a bar plot
+# to summarize the ratios of politician appearances across the different tv-shows.
+
+
+
+
